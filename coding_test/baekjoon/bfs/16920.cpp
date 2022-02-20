db99e745d09a2c260aabfca0de1598c9514cb8c0
kimseunghyun <seunghyun4525@gmail.com>
2022-02-21 03:05:11 +0900

    Coding Test BOJ 16920

--- coding_test/baekjoon/bfs/16920.cpp
+++ coding_test/baekjoon/bfs/16920.cpp
@@ -3,21 +3,22 @@ Time : 2022.02.20
 Problem : BOJ16920 (https://www.acmicpc.net/problem/16920)
 Algorithm Type : BFS
 Outline :
-
+NxM 크기의 격자판이 비어있거나(.), 벽이 있다(#)
+해당 격자판 위에서 "확장 게임"을 진행하려고 한다.
+확장 게임은 플레이어별로 성을 가지고 있고, 플레이어 1부터 마지막 플레이어까지 확장을 한다.
+확장이란 자신이 가지고 있는 성에서 Si칸 만큼 이동할 수 있는 모든 칸에 성을 동시에 만든다.
+(벽 또는 다른 플레이어의 성으로는 확장할 수 없다.)
+모든 플레이어가 더이상 확장을 할 수 없을 때 까지 게임을 진행한다.
+초기 게임판이 주어졌을 때 최종 상태를 출력한다. (플레이어별로 가진 성의 수를 출력)
 Method :
-이동 거리를 저장하는 배열을 낮, 밤 따로 정한다.
-dist_noon[i][j][k]는 벽을 k번 부셔서 낮에 (i,j)로 도착하는 최단 경로,
-dist_night[i][j][k]는 벽을 k번 부셔서 밤에 (i,j)로 도착하는 최단 경로를 저장한다.
-매 BFS 시행마다 낮->밤, 밤->낮이 바뀐다.
-또한 기존에는 BFS시 a[i][j] == 1 (벽) 이라면 진행하지 않았는데,
-만약 제자리에 서 있는 경우 처리를 해 주어야 하기 때문에,
-nx == x && ny == y 인 경우로 따로 처리를 해 주었다.
-이외에는 이전 벽부수기 문제와 비슷하므로 생략한다.
 Example :
-1 4 1
-0010
+3 3 2
+1 1
+1..
+...
+..2
 -------
-15
+6 3
 */
 #include <bits/stdc++.h>
 using namespace std;
@@ -66,110 +67,97 @@ bool outOfBound0(int x, int y, int m, int n) {
   return (x<0 || y<0 || m<=x || n<=y);
 }
 
-
 bool outOfBound1(int x, int y, int m, int n) {
   // 1-indexed oob
   return (x<1 || y<1 || m<x || n<y);
 }
 
-int n, m, k;
-int a[1000+5][1000+5];
-int dist_noon[1000+5][1000+5][10+5];
-int dist_night[1000+5][1000+5][10+5];
+int charToInt(char c) {
+  if ('A' <= c && c <= 'Z')
+    return (int)(c - 'A');
+  else if ('a' <= c && c <= 'z')
+    return (int)(c - 'a');
+  else if ('0' <= c && c <= '9')
+    return (int)(c - '0');
+  return -1;
+}
+
+int n, m, p;
+string a[1000+5];
+int castle[1000+5][1000+5];
+int dist[1000+5][1000+5];
+int how_move[10+5];
+int turn[10+5];
 
 int main(void) {
   ios::sync_with_stdio(0); cin.tie(0);
-  string tmp;
-  const int mx = 1000000+5;
-  cin >> n >> m >> k;
+  cin >> n >> m >> p; // 세로x가로, 플레이어 수
+  vector<pii> stt(p+5);
+  queue<int> q1;
+  queue<pii> q2;
+  vector<vector<pii> > v(p+1);
+  for (int i=1; i<=p; i++) {
+    int s;
+    cin >> s;
+    how_move[i] = s;
+  }
   for (int i=0; i<n; i++) {
-    cin >> tmp;
+    cin >> a[i];
     for (int j=0; j<m; j++) {
-      a[i][j] = (tmp[j]=='0'?0:1);
-    }
-  }
-
-  fill(&dist_night[0][0][0], &dist_night[1000+5-1][1000+5-1][10+5], mx);
-  fill(&dist_noon[0][0][0], &dist_noon[1000+5-1][1000+5-1][10+5], mx);
-  queue<pair<pii, pii> > q; // ((좌표),(부순횟수,낮(1)/밤(0))) 
-  dist_noon[0][0][0] = 1;
-  q.push(mp(mp(0,0), mp(0,1)));
-
-  while(!q.empty()) {
-    int x = q.front().first.X;
-    int y = q.front().first.Y;
-    int bk = q.front().second.first;
-    int is_noon = q.front().second.second;
-
-    q.pop();
-
-    for (int i=0; i<5; i++) {
-      int nx = x + dx[i];
-      int ny = y + dy[i];
-
-
-
-      if (i==4) { // 이동하지 않음
-        nx = x;
-        ny = y;
-      }
-      
-      if (outOfBound0(nx, ny, n, m)) continue;
-      if (a[nx][ny] == 0) {
-        // 벽이 아닌 경우 -> 이동 가능
-        // 만약 벽을 0번 ~ 현재까지 부순 횟수번 사이에서
-        // 더 가깝게 도착할 수 있다면 탐색X
-        int flag = 0;
-        if (is_noon) {
-          if (dist_noon[x][y][bk] + 1 < dist_night[nx][ny][bk]) {
-            q.push(mp(mp(nx,ny), mp(bk,0)));
-            dist_night[nx][ny][bk] = dist_noon[x][y][bk] + 1;
-          }
-        } else {
-          if (dist_night[x][y][bk] + 1 < dist_noon[nx][ny][bk]) {
-            q.push(mp(mp(nx,ny), mp(bk,1)));
-            dist_noon[nx][ny][bk] = dist_night[x][y][bk] + 1;
-          }
-        }
-      } else { // 벽 위인 경우
-        if (bk == k) continue;
-        else if ((x==nx) && (y==ny)) {
-          if (is_noon) {
-            if (dist_noon[x][y][bk] + 1 < dist_night[nx][ny][bk]) {
-              q.push(mp(mp(nx,ny), mp(bk,0)));
-              dist_night[nx][ny][bk] = dist_noon[x][y][bk] + 1;
-            }
-          } else {
-            if (dist_night[x][y][bk] + 1 < dist_noon[nx][ny][bk]) {
-              q.push(mp(mp(nx,ny), mp(bk,1)));
-              dist_noon[nx][ny][bk] = dist_night[x][y][bk] + 1;
-            }
-          }
-        } else if (!is_noon) {
-          continue;
-        } else if (dist_noon[x][y][bk] + 1 < dist_night[nx][ny][bk+1]) {
-          // 벽을 부수고 이동하는 경우 탐색
-          q.push(mp(mp(nx,ny), mp(bk+1,0)));
-          dist_night[nx][ny][bk+1] = dist_noon[x][y][bk] + 1;
-        }
+      if ('1' <= a[i][j] && a[i][j] <= '9') {
+        castle[i][j] = charToInt(a[i][j]);
+        v[charToInt(a[i][j])].push_back(mp(i,j));
       }
     }
   }
 
-  int ans = mx;
-  for (int i=0; i<=k; i++) {
-    ans = min(ans, dist_night[n-1][m-1][i]);
-    ans = min(ans, dist_noon[n-1][m-1][i]);
-    // cout << i << "\n";
-    // for (int j=0; j<n; j++) {
-    //   for (int k=0; k<m; k++) {
-    //     cout << dist[j][k][i] << " ";
-    //   }
-    //   cout << "\n";
-    // }
-    // cout << "\n";
+  for (int i=1; i<=p; i++) {
+    q1.push(i);
   }
-  cout << (ans==mx?-1:ans);
-
+  
+  while(!q1.empty()) {
+    int player = q1.front();
+    fill(&dist[0][0], &dist[1000+5-1][1000+5], 1000*1000+5);
+    q1.pop();
+    for (auto pi : v[player]) {
+      dist[pi.X][pi.Y] = 0;
+      castle[pi.X][pi.Y] = player;
+      q2.push(pi);
+    }
+    v[player].clear();
+    if (q2.empty()) continue;
+    while(!q2.empty()) {
+      int x = q2.front().X;
+      int y = q2.front().Y;
+      int move = dist[x][y];
+      q2.pop();
+      if (move == how_move[player]) {
+        v[player].push_back(mp(x,y));
+        continue;
+      }
+      for (int i=0; i<4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if (outOfBound0(nx,ny,n,m)) continue;
+        else if (castle[nx][ny] != 0 || a[nx][ny]=='#') continue; // 이미 지나왔거나, 다른 사람 영역
+        else if (move + 1 < dist[nx][ny]) {
+          // 최단 경로일 경우
+          q2.push(mp(nx,ny));
+          dist[nx][ny] = move + 1;
+          castle[nx][ny] = player;
+        }
+      }
+    }
+    q1.push(player);
+  }
+  int ans[10] = {0};
+  for (int i=0; i<n; i++) {
+    for (int j=0; j<m; j++) {
+      ans[castle[i][j]]++;
+    }
+  }
+  for (int i=1; i<=p; i++) {
+    cout << ans[i] << " ";
+  }
   return 0;
 }
